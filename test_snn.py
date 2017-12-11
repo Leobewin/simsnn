7214670ec897461a88b1e11b655bb5a74306cb9f
ysingh7 <yashnitkkr7@gmail.com>
2017-12-11 09:25:01 -0800

    Added the test file

---
+++ test_snn.py
@@ -0,0 +1,31 @@
+from snn import SpikingNueralNetwork
+import numpy as np
+import unittest
+ 
+class TestSNN(unittest.TestCase):
+    """
+    Test the varoius components of SNN class 
+    """
+    def test_input_next_iteration_reset(self):
+        """
+        Test that all the  values of x are being reset if y is not above
+        threshold
+        """
+        network = SpikingNueralNetwork(input=3,output=3,threshold=15)
+        network.feed_forward()
+        network.after_forward()
+        self.assertEqual(np.any(network.x),False)
+ 
+    def test_output_reset_after_threshold(self):
+        """
+        Test that all the  values of x are being reset if y is not above
+        threshold
+        """
+        network = SpikingNueralNetwork(input=3,output=3,threshold=5)
+        network.y = np.array([4.0,5.0,1]).reshape(3,1)
+        network.feed_forward()
+        network.after_forward()
+        self.assertEqual(network.y[1],0)
+
+if __name__ == '__main__':
+    unittest.main()
