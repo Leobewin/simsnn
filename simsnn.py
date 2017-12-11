02b055c956d4c4d1fbb3956aa90a239d888428a6
ysingh7 <yashnitkkr7@gmail.com>
2017-12-10 18:03:24 -0800

    Working parallel Implementation

--- simsnn.py
+++ simsnn.py
@@ -3,24 +3,84 @@ import argparse
 from snn import SpikingNueralNetwork
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing import Process,Queue
 
 # Version of the Simulator
 SIMSNN_VER = "0.1"
 
-def runsim(xseed, wseed, bseed, tsim):
-    network = SpikingNueralNetwork(inputseed=xseed,weightseed=wseed,biasseed=bseed,threshold=12)
-    network_state = []
-    for i in range(tsim):
-        network.feed_forward()
-        network.after_forward()
-        network_state.append(network.y)
+def simulation(network, tsim, worker_index, chunk_boundary, result_queue,x_queue):
+    network.feed_forward_parallel(*chunk_boundary)
+    nueron_fired = network.after_forward_parallel(*chunk_boundary)
+    #Update the value of x in the shared queue
+    x_queue.put((worker_index,network.x))
+    #Put the value of y in the queue for aggregation
+    result_queue.put((worker_index,network.y))
 
-    network_state = np.array(network_state)
-    x,y,z = network_state.shape
-    network_state = network_state.reshape(x,y)
-    plt.plot(network_state[:,0])
-    plt.axis([0,100,0,12])
+
+
+def runsim(xseed, wseed, bseed, tsim, number_of_process=2):
+    network = SpikingNueralNetwork(inputseed=xseed,weightseed=wseed,biasseed=bseed,threshold=5)
+    network_output_state = []
+    network_input_state = []
+    print(network)
+    work_divison = network.output//number_of_process
+    chunk_boundaries = [(i*work_divison,i*work_divison+work_divison) for i in range(number_of_process)]
+    if(network.output%number_of_process):
+        chunk_boundaries[-1]=(chunk_boundaries[-1][0],network.output)
+
+    for i in range(tsim):
+        children = []
+        result_queue = Queue()
+        x_queue = Queue()
+        for worker_index in range(number_of_process):
+            children.append(
+                Process(
+                    target=simulation,
+                    args=(
+                        network,
+                        tsim,
+                        worker_index,
+                        chunk_boundaries[worker_index],
+                        result_queue,
+                        x_queue
+
+                    )
+                )
+            )
+
+        for child in children:
+            child.start()
+
+        for worker_index in range(number_of_process):
+            worker_index, result_chunk = result_queue.get(block=True)
+            chunk_boundary = chunk_boundaries[worker_index]
+            network.y[chunk_boundary[0]:chunk_boundary[1],:] = result_chunk[chunk_boundary[0]:chunk_boundary[1],:]
+
+        for worker_index in range(number_of_process):
+            if not x_queue.empty():
+                worker_index, x = x_queue.get()
+                chunk_boundary = chunk_boundaries[worker_index]
+                network.x[chunk_boundary[0]:chunk_boundary[1],:] = x[chunk_boundary[0]:chunk_boundary[1],:]
+
+        for c in children:
+            c.join()
+        network_output_state.append(network.y.copy())
+        network_input_state.append(network.x.copy())
+        #print(network_output_state)
+        #print(network_input_state)
+    #print(network_output_state)
+    print(network_input_state)
+    network_state_y = np.array(network_output_state)
+    network_state_x = np.array(network_input_state)
+    x,y,z = network_state_y.shape
+    network_state_y = network_state_y.reshape(x,y)
+    plt.plot(network_state_y[:,0])
     plt.ylabel('State of Nueral Network')
+    plt.show()
+    x,y,z = network_state_x.shape
+    network_state_x = network_state_x.reshape(x,y)
+    plt.plot(network_state_x[:,0])
+    plt.ylabel('Input State of Nueral Network')
     plt.show()
 
 
@@ -40,7 +100,7 @@ if __name__ == "__main__":
                             default=0, help='Seed value for bias')
 
     arg_parser.add_argument('-t','--tsim',action='store',type=int,
-                            default=100, help='Total number of time steps to simulate')
+                            default=50, help='Total number of time steps to simulate')
 # Parse all the arguments
     args = arg_parser.parse_args(sys.argv[1:])
 # Running the simulator for timestep tsim
--- snn.py
+++ snn.py
@@ -2,7 +2,9 @@ import numpy as np
 from utils import *
 
 class SpikingNueralNetwork:
-    def __init__(self, input=1000, output=1000, inputseed=7000, weightseed=8000, biasseed=0, activation=sigmoid, threshold=50):
+    def __init__(self, input=10, output=10, inputseed=7000, weightseed=8000, biasseed=0, activation=sigmoid, threshold=1):
+        self.input=input
+        self.output=output
         np.random.seed=inputseed
         self.x = np.random.randint(2,size=(input,1))
         np.random.seed=weightseed
@@ -14,16 +16,63 @@ class SpikingNueralNetwork:
         self.threshold = threshold
 
     def __repr__(self):
-        return "x is : {}\n Weight Matrix is : {}\n Bias is : {}\n y is : {}\n ".format(self.x,self.W,self.b,self.y)
+        return "x is : {}\n y is : {}\n ".format(self.x,self.y)
 
     def feed_forward(self):
         self.y = self.y + self.activation(np.matmul(self.W,self.x)+self.b)
+
+    def feed_forward_parallel(self,start,end):
+        self.y[start:end,:] = self.y[start:end,:] + self.activation(np.matmul(self.W[start:end,:],self.x)+self.b[start:end,:])
+
+    def after_forward_parallel(self,start,end):
+        above_threshold = self.y[start:end,:]>self.threshold
+        self.y[start:end,:][above_threshold]=0
+        self.x[start:end,:][above_threshold]=1
+        self.x[start:end,:][above_threshold<1]=0
+        if np.any(above_threshold):
+            return True
+        return False
+
 
     def after_forward(self):
         above_threshold = self.y>self.threshold
         self.y[above_threshold]=0
         self.x[above_threshold]=1
         self.x[above_threshold<1]=0
+        if np.any(above_threshold):
+            return True
+        return False
+
+    def get_weight_matrix(self):
+        return self.W
+
+    def get_x(self):
+        return self.x
+
+    def get_bias(self):
+        return self.b
+
+    def get_output(self):
+        return self.y
+
+    def print_weight_matrix(self):
+        print("Weight Matrix is : {}".format(self.W))
+
+    def print_bias(self):
+        print("Bias is : {}".format(self.b))
+
+    def print_x(self):
+        print("X is : {}".format(self.x))
+
+    def print_y(self):
+        print("y is : {}".format(self.y))
+
+    def debug_network_state(self):
+        self.print_weight_matrix()
+        self.print_bias()
+        self.print_x()
+        self.print_y()
+
 
 
 
